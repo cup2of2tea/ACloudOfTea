630d062108ac616e1154fc23b541c5ca6fc25e0f
unknown <comtealexis@gmail.com>
2020-07-26 20:29:12 +0200

    use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'articles/WhereIsGuido', component: WhereIsGuidoComponent }
 ];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
