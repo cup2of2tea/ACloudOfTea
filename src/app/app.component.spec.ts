5d6a0a3e5ea2aaf1aaf2c7f6ad9a092f0147de11
unknown <comtealexis@gmail.com>
2020-07-26 12:49:47 +0200

    Test

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,14 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { TouchCheckboxComponent } from './touch-checkbox/touch-checkbox.component';
+import { TouchInputSpinnerComponent } from './touch-input-spinner/touch-input-spinner.component';
+import { DollarAmountInputComponent } from './dollar-amount-input/dollar-amount-input.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+describe('AppComponent', () => {
+
+
+});
