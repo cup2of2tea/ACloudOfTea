61cfa664665402df4fba089ba0dfd09327edf7b7
Comte Alexis <alex6ssh@hotmail.fr>
2021-12-12 12:28:28 +0100

    Migration

--- src/app/Algorithmique/AoC/2021/day1/day1.component.ts
+++ src/app/Algorithmique/AoC/2021/day1/day1.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component, ElementRef, Input, HostListener, NgZone, OnDestroy } from '@angular/core';
+import { Application, IApplicationOptions } from 'pixi.js';
 import { NgbNavChangeEvent } from '@ng-bootstrap/ng-bootstrap';
+
+
 declare var $: any;
 declare var Prism: any;
 
@@ -10,7 +13,16 @@ declare var Prism: any;
 })
 export class Day1_2021Component implements OnInit {
 
-  constructor() { }
+  public app: Application;
+
+
+  //@Input()
+  public devicePixelRatio = window.devicePixelRatio || 1;
+
+  //@Input()
+  public applicationOptions: IApplicationOptions = {};
+
+  constructor(private elementRef: ElementRef, private ngZone: NgZone) {}
 
   isCollapsed: any;
   isCollapsed2: any;
@@ -20,20 +32,37 @@ export class Day1_2021Component implements OnInit {
   nav2: any;
 
   ngOnInit() {
-    this.isCollapsed = false;
-    this.isCollapsed2 = false;
-    this.isCollapsed3 = false;
-    this.isCollapsed4 = false;
 
     setTimeout(() => { Prism.highlightAll(); }, 10);
+
+    this.ngZone.runOutsideAngular(() => {
+      this.app = new Application(this.applicationOptions);
+    });
+    this.elementRef.nativeElement.appendChild(this.app.view);
+    this.resize();
   }
 
   onNavChange(changeEvent: NgbNavChangeEvent) {
     setTimeout(() => { Prism.highlightAll(); }, 10);
   }
-  goto(selector) {
-    document.querySelector(selector).scrollIntoView({ behavior: 'smooth' })
 
+  @HostListener('window:resize')
+  public resize() {
+    const width = this.elementRef.nativeElement.offsetWidth;
+    const height = this.elementRef.nativeElement.offsetHeight;
+    const viewportScale = 1 / this.devicePixelRatio;
+    this.app.renderer.resize(width * this.devicePixelRatio, height * this.devicePixelRatio);
+    this.app.view.style.transform = `scale(${viewportScale})`;
+    this.app.view.style.transformOrigin = `top left`;
   }
+
+  destroy() {
+    this.app.destroy();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy();
+  }
+
 
 }
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -61,3 +61,26 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+
+ interface HTMLCanvasElement extends HTMLElement {
+    // ...
+    transferControlToOffscreen(): OffscreenCanvas;
+}
+
+interface OffscreenCanvasRenderingContext2D extends CanvasState, CanvasTransform, CanvasCompositing, CanvasImageSmoothing, CanvasFillStrokeStyles, CanvasShadowStyles, CanvasFilters, CanvasRect, CanvasDrawPath, CanvasUserInterface, CanvasText, CanvasDrawImage, CanvasImageData, CanvasPathDrawingStyles, CanvasTextDrawingStyles, CanvasPath {
+    readonly canvas: OffscreenCanvas;
+}
+declare var OffscreenCanvasRenderingContext2D: {
+    prototype: OffscreenCanvasRenderingContext2D;
+    new(): OffscreenCanvasRenderingContext2D;
+}
+interface OffscreenCanvas extends EventTarget {
+    width: number;
+    height: number;
+    getContext(contextId: "2d", contextAttributes?: CanvasRenderingContext2DSettings): OffscreenCanvasRenderingContext2D | null;
+}
+declare var OffscreenCanvas: {
+    prototype: OffscreenCanvas;
+    new(width: number, height: number): OffscreenCanvas;
+}
